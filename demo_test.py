0890a49080c7e0519ce0361f5354456e90abb36f
dominic@data-science-hk.org <dominic@data-science-hk.org>
2023-12-21 21:22:15 +0800

    demo test

--- demo_test.py
+++ demo_test.py
@@ -24,6 +24,7 @@ import logging
 from PIL import Image
 from PIL import ImageFile
 import os
+from patchify import patchify, unpatchify
 
 
 def demo_test(args, TestImgLoader, model, save_path, device):
@@ -70,7 +71,7 @@ def load_checkpoint(model):
         raise NotImplementedError
     mkdir(save_path)
     if load_path.endswith('.pth'):
-        model_state_dict = torch.load(load_path)
+        model_state_dict = torch.load(load_path, map_location=torch.device('cpu') )
     else:
         model_state_dict = torch.load(load_path)['state_dict']
     model.load_state_dict(model_state_dict)
@@ -97,10 +98,9 @@ def set_logging(log_path):
 
 def test_model_fn(args, data, model, save_path, device):
     # prepare input and forward
-    in_img = data['in_img'].to(device)
+    in_img = data['in_img']
     number = data['number']
     b, c, h, w = in_img.size()
-
     # pad image such that the resolution is a multiple of 32
     w_pad = (math.ceil(w/32)*32 - w) // 2
     h_pad = (math.ceil(h/32)*32 - h) // 2
@@ -120,10 +120,10 @@ def test_model_fn(args, data, model, save_path, device):
         if w_pad != 0:
             out_1 = out_1[:, :, :, w_pad:-w_odd_pad]
 
-    # save images
-    if args.SAVE_IMG:
-        out_save = out_1.detach().cpu()
-        torchvision.utils.save_image(out_save, save_path + '/' + 'test_%s' % number[0] + '.%s' % args.SAVE_IMG)
+    # # save images
+    # if args.SAVE_IMG:
+    out_save = out_1.detach().cpu()
+    torchvision.utils.save_image(out_save, save_path + '/' + 'test_%s' % number[0] + '.%s' % args.SAVE_IMG)
 
 def create_demo_dataset(
     args,
@@ -180,7 +180,7 @@ def main():
                      de_feature_num=args.DE_FEATURE_NUM,
                      de_inter_num=args.DE_INTER_NUM,
                      sam_number=args.SAM_NUMBER,
-                     ).to(device)
+                     )
 
     # load checkpoint
     load_path, save_path, log_path = load_checkpoint(model)
@@ -198,14 +198,11 @@ def main():
     # Set test batch size to 1 for avoiding OOM
     args.BATCH_SIZE = 1
     DemoImgLoader = create_demo_dataset(args, data_path=test_path)
-
     # test demo
-
     demo_test(args, DemoImgLoader, model, save_path, device)
 
 
 if __name__ == '__main__':
-    print(torch.cuda.device_count())
     main()
 
 
